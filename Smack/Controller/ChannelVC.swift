0ff152314dc7cf406571b9e4ee3ab072139be85f
Raghav Vashisht <raghav2011rv@gmail.com>
2017-07-20 12:29:58 +0530

    Implemented SWRevealViewController for sliding menu

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,18 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Raghav Vashisht on 20/07/17.
+//  Copyright © 2017 Raghav Vashisht. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+    }
+}
