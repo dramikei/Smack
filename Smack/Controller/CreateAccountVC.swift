7fd590f5033a9ffa8c735b525b99b77a78620690
Raghav Vashisht <raghav2011rv@gmail.com>
2017-07-20 22:54:45 +0530

    Added CreateAccount UI

---
+++ Smack/Controller/CreateAccountVC.swift
@@ -0,0 +1,24 @@
+//
+//  CreateAccountVC.swift
+//  Smack
+//
+//  Created by Raghav Vashisht on 20/07/17.
+//  Copyright © 2017 Raghav Vashisht. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+    
+}
