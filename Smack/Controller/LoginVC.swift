408b789ecddbeb01afa41c65dc3fc8bb02aae913
Raghav Vashisht <raghav2011rv@gmail.com>
2017-07-20 22:28:41 +0530

    Added Login UI and set up segues

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVCViewController.swift
+//  Smack
+//
+//  Created by Raghav Vashisht on 20/07/17.
+//  Copyright © 2017 Raghav Vashisht. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func cancelBtnPressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Raghav Vashisht on 20/07/17.
+//  Copyright © 2017 Raghav Vashisht. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
