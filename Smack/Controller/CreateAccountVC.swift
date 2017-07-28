50b803b150f85e6274d1f75402577748056fe6b2
Raghav Vashisht <raghav2011rv@gmail.com>
2017-07-28 23:31:20 +0530

    Finished CreateUserAccount section

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -15,13 +15,17 @@ class CreateAccountVC: UIViewController {
     
     @IBOutlet weak var profileImg: UIImageView!
     
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    
     // Variables
     
     var avatarName = "profileDefault"
     var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    var bgColor : UIColor?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
         // Do any additional setup after loading the view.
     }
     
@@ -29,10 +33,15 @@ class CreateAccountVC: UIViewController {
         if UserDataService.instance.avatarName != "" {
             profileImg.image = UIImage(named: UserDataService.instance.avatarName)
             avatarName = UserDataService.instance.avatarName
+            if avatarName.contains("light") && bgColor == nil {
+                profileImg.backgroundColor = UIColor.lightGray
+            }
         }
     }
     
     @IBAction func createAccountPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
         guard let username = usernameTxt.text , usernameTxt.text != "" else { return }
         guard let email = emailTxt.text , emailTxt.text != "" else { return }
         guard let pass = passwordTxt.text , passwordTxt.text != "" else { return }
@@ -43,7 +52,10 @@ class CreateAccountVC: UIViewController {
                     if success {
                         AuthService.instance.createUser(name: username, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
                             if success {
+                                self.spinner.isHidden = true
+                                self.spinner.stopAnimating()
                                 self.performSegue(withIdentifier: UNWIND, sender: nil)
+                                NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
                             }
                         })
                     }
@@ -57,11 +69,30 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func pickBGColorPressed(_ sender: Any) {
+        let red = CGFloat(arc4random_uniform(255)) / 255
+        let green = CGFloat(arc4random_uniform(255)) / 255
+        let blue = CGFloat(arc4random_uniform(255)) / 255
+        
+        bgColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        UIView.animate(withDuration: 0.2) {
+            self.profileImg.backgroundColor = self.bgColor
+        }
     }
-    
-    
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
-
+    
+    func setupView() {
+        spinner.isHidden = true
+        usernameTxt.attributedPlaceholder = NSAttributedString(string: "username", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceHolder])
+        emailTxt.attributedPlaceholder = NSAttributedString(string: "email", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceHolder])
+        passwordTxt.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceHolder])
+        let tap = UITapGestureRecognizer(target: self, action: #selector(CreateAccountVC.handleTap))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap() {
+        view.endEditing(true)
+    }
+    
 }
---
+++ Smack/View/CircleImg.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImg.swift
+//  Smack
+//
+//  Created by Raghav Vashisht on 28/07/17.
+//  Copyright © 2017 Raghav Vashisht. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImg: UIImageView {
+    
+    override func awakeFromNib() {
+        
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+    
+}
