56487603dbc012559dd00254d0c7d6062d1faee8
Raghav Vashisht <raghav2011rv@gmail.com>
2017-07-30 20:54:06 +0530

    Completed Logged in Interface

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -74,6 +74,7 @@ class CreateAccountVC: UIViewController {
         let blue = CGFloat(arc4random_uniform(255)) / 255
         
         bgColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        avatarColor = "[\(red), \(green), \(blue), \(1)]"
         UIView.animate(withDuration: 0.2) {
             self.profileImg.backgroundColor = self.bgColor
         }
